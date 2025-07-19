3aab99827b7fa884252d1d40db7b5b12da921924
Brook Aberra <brookaberra30@gmail.com>
2025-07-18 20:47:34 -0500

    Initial Commit

---
+++ HelloTools Dashboard/HelloTools_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTools_DashboardApp.swift
+//  HelloTools Dashboard
+//
+//  Created by broke on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloTools_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
